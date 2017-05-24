5517927684958f09c3d6485e5d48f474fbfad9e2
Steve Vissault <s.vissault@yahoo.fr>
2017-05-24 09:36:57 -0400

    Push new slides: propriétés des neouds

--- src/graph.r
+++ src/graph.r
@@ -5,12 +5,14 @@ library("igraph")
 devtools::load_all("../alien")
 
 pair <- bartomeus$interactPair
-pair <- subset(pair,strength>=20) # Prob: séléctionne que les individus de plantes avec 50 interact et plus
-
 # prep species
 idObs <- as.data.frame(unique(bartomeus$idObs[,c("idSp","idInd")]))
 idObs$sp <- as.character(idObs$idSp)
 idObs$idSp <- as.numeric(idObs$idSp)
 
-idSpFrom <- merge(pair,idObs, all.x=TRUE,by.x="idFrom",by.y="idInd")[,"sp"]
-idSpTo <- merge(pair,idObs, all.x=TRUE,by.x="idTo",by.y="idInd")[,"sp"]
+pair$idSpFrom <- merge(pair,idObs, all.x=TRUE,by.x="idFrom",by.y="idInd")[,"sp"]
+pair$idSpTo <- merge(pair,idObs, all.x=TRUE,by.x="idTo",by.y="idInd")[,"sp"]
+
+pair <- pair[,c('idSpTo','idSpFrom','strength')]
+pair <- aggregate(strength ~ idSpTo + idSpFrom, data=pair, FUN=sum)
+apis <- subset(pair, idSpTo == 'Apis mellifera')
